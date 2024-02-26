6cc2e12e7817e757329844c5f6c1b60702f6d7ca
Caio Gazen <61031028+CaioGazen@users.noreply.github.com>
2024-02-26 20:37:55 -0300

    formating

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ import os
 COEF_APRENDIZADO = 0.5
 
 N_DIGITOS = 2
- 
+
 
 class Neoronio:
     def __init__(self, numeroSinapses):
-        self.sinapses = np.array([random.randint(0,1) for i in range(numeroSinapses)])
+        self.sinapses = np.array([random.randint(0, 1)
+                                 for i in range(numeroSinapses)])
 
     def calcularV(self, vetorEntrada):
         self.v = np.dot(self.sinapses, vetorEntrada)
@@ -23,9 +24,11 @@ class Neoronio:
 
     def calcularErro(self, yDesejado):
         self.erro = yDesejado - self.y
-        
+
     def atualizarSinapses(self, vetorEntrada):
-        self.sinapses = (COEF_APRENDIZADO * self.erro * vetorEntrada) + self.sinapses
+        self.sinapses = (COEF_APRENDIZADO * self.erro *
+                         vetorEntrada) + self.sinapses
+
 
 class Dados:
     def __init__(self, vetorEntrada, yDesejado):
@@ -42,9 +45,10 @@ def calcularErroMedioVetorEntrada(errosNeoronios):
     erroMedioVetorEntrada = erroMedioVetorEntrada/len(errosNeoronios)
     return erroMedioVetorEntrada
 
+
 def calcularErroEpoca(errosMediosVetorEntrada):
     erroEpoca = 0
-    
+
     for i in range(len(errosMediosVetorEntrada)):
         erroEpoca += errosMediosVetorEntrada[i]
 
@@ -52,19 +56,18 @@ def calcularErroEpoca(errosMediosVetorEntrada):
     return erroEpoca
 
 
-
-
 conjuntoTreino = []
+
 
 def carregarImagensTreino():
     i = 0
-    while os.path.exists('digits_training/'+ str(i)) and i < N_DIGITOS:
-        print('digits_training/'+ str(i))
-        path = 'digits_training/'+ str(i) + '/'
+    while os.path.exists('digits_training/' + str(i)) and i < N_DIGITOS:
+        print('digits_training/' + str(i))
+        path = 'digits_training/' + str(i) + '/'
 
         yDesejado = [0 for k in range(N_DIGITOS)]
         yDesejado[i] = 1
-        
+
         j = 0
         while os.path.isfile(path + str(j) + '.png'):
             image = imread(path + str(j) + '.png')
@@ -72,7 +75,7 @@ def carregarImagensTreino():
             image = image.ravel()
 
             conjuntoTreino.append(Dados(image, yDesejado))
-            
+
             j += 1
 
         i += 1
@@ -82,8 +85,8 @@ def classificarTeste():
     resultados = []
 
     i = 0
-    while os.path.exists('digits_examples/'+ str(i)) and i < N_DIGITOS:
-        path = 'digits_examples/'+ str(i) + '/'
+    while os.path.exists('digits_examples/' + str(i)) and i < N_DIGITOS:
+        path = 'digits_examples/' + str(i) + '/'
 
         yDesejado = [0 for k in range(N_DIGITOS)]
         yDesejado[i] = 1
@@ -97,34 +100,40 @@ def classificarTeste():
             dado = Dados(image, yDesejado)
 
             resultados.append([dado, classificar(neoronios, dado)])
-            
+
             j += 1
-        
+
         i += 1
 
     return resultados
 
+
 teste = {
-    str([0,0]): "nenhum ",
-    str([0,1]): "   1   ",
-    str([1,0]): "   0   ",
-    str([1,1]): "  0,1  "
+    str([0, 0]): "nenhum ",
+    str([0, 1]): "   1   ",
+    str([1, 0]): "   0   ",
+    str([1, 1]): "  0,1  "
 }
+
 
 def compararResultados(resultados):
     print("  Valor Real | Classificacao | indice ")
     for i in range(len(resultados)):
-        print("  ",teste[str(resultados[i][0].yDesejado)],"  |   ", teste[str(resultados[i][1])],"   | ", i)
+        print("  ", teste[str(resultados[i][0].yDesejado)],
+              "  |   ", teste[str(resultados[i][1])], "   | ", i)
+
 
 neoronios = [Neoronio(16384), Neoronio(16384)]
 
-#neoronio[0].sinapses = np.array([0,0,1,1,1,0,0,0,0,0])
-#neoronio[1].sinapses = np.array([1,1,1,1,1,0,0,0,0,0])
-#neoronio[2].sinapses = np.array([0,0,0,0,0,1,1,1,1,1])
+# neoronio[0].sinapses = np.array([0,0,1,1,1,0,0,0,0,0])
+# neoronio[1].sinapses = np.array([1,1,1,1,1,0,0,0,0,0])
+# neoronio[2].sinapses = np.array([0,0,0,0,0,1,1,1,1,1])
+
+
 def treinar(neoronios, conjuntoTreino):
 
     erroEpoca = 1
-    while(erroEpoca >= 0.00001): 
+    while (erroEpoca >= 0.00001):
         errosMediosVetorEntrada = []
         for i in range(len(conjuntoTreino)):
             errosNeoronios = []
@@ -142,10 +151,12 @@ def treinar(neoronios, conjuntoTreino):
                 print(neoronios[j].sinapses)
 
             print(errosNeoronios)
-            errosMediosVetorEntrada.append(calcularErroMedioVetorEntrada(errosNeoronios))
+            errosMediosVetorEntrada.append(
+                calcularErroMedioVetorEntrada(errosNeoronios))
 
         erroEpoca = calcularErroEpoca(errosMediosVetorEntrada)
         print(erroEpoca)
+
 
 def classificar(neoronios, dados):
     resultado = []
@@ -157,26 +168,26 @@ def classificar(neoronios, dados):
         resultado.append(neoronios[i].y)
     return resultado
 
+
 def live(neoronios):
     sair = False
     while not sair:
-            while os.path.isfile("live.sig") == False:
-                pass
-            
-            os.remove("live.sig")
-            
-            image = imread("live.png")
-            image = np.sum(image, 2)
-            image = image.ravel()
+        while not os.path.isfile("live.sig"):
+            pass
 
-            yDesejado = 0
+        os.remove("live.sig")
 
-            dado = Dados(image, yDesejado)
+        image = imread("live.png")
+        image = np.sum(image, 2)
+        image = image.ravel()
 
-            resultado = classificar(neoronios, dado)
-            print(resultado)
-            print(teste[str(resultado)])
+        yDesejado = 0
 
+        dado = Dados(image, yDesejado)
+
+        resultado = classificar(neoronios, dado)
+        print(resultado)
+        print(teste[str(resultado)])
 
 
 sair = False
@@ -198,7 +209,7 @@ while not sair:
             resultados = classificarTeste()
             compararResultados(resultados)
 
-        case "3": 
+        case "3":
             sair = True
 
         case "4":
